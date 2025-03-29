66fef29307d6b419f8201b044a7c52ee6da8ef8a
hoafan <Phannhathoa13@gmail.com>
2025-03-29 10:49:19 +0700

    doneTask

--- learning-react/src/App.js
+++ learning-react/src/App.js
@@ -1,56 +1,44 @@
-import React from "react";
-import { useState } from "react";
-const listroducts = [
-  { title: "Cabbage", isFruit: false, amount: 20, id: 1 },
-  { title: "Garlic", isFruit: false, amount: 30, id: 2 },
-  { title: "Apple", isFruit: true, amount: 40, id: 3 },
-];
+import { useState } from "react"
 
-function App() {
-  const listItems = listroducts.map(
-    (
-      product // Key dùng để Trích tới thẳng ví trị hoặc id của phần tử đó mà ko cần reRender lại cả trang
-    ) => (
-      <li key={product.id} style={{ color: product.isFruit ? "blue" : "red" }}>
-        <strong>{product.title}</strong> - Amount: {product.amount}
-      </li>
+function DisplayMyNameAndDate() {
+  const name = "Nhat Hoa"
+  return (
+    <>
+      <div>
+        <h1>{name}</h1>
+        <h2>Date: 2004</h2>
+      </div>
+    </>
+  )
+}
+
+function CreateSpaceDOM() {
+  return (
+    <>
+      <div>
+        <div>
+          <p>
+            <p>
+            </p>
+          </p>
+        </div>
+      </div>
+    </>
+  )
+}
+
+function DisplayTrueFlag() {
+  const isLogIn = true;
+  if (isLogIn) {
+    return (
+      <>
+        {`${isLogIn}`}
+      </>
     )
-  );
-  console.log(listItems);
-
-  return (
-    // Dấu <> Nó là 1 fragment, có nghĩa là nó sẽ ko tạo 1 phần tử cha ra để đỡ tốn tài nguyên
-    <>
-      <h1>Product List</h1>
-      <ul>{listItems}</ul>
-    </>
-  );
-}
-
-function clickMeButton() {
-  window.alert("you clicked me");
-}
-function MyButton() {
-  return (
-    // Khi báo function trong jsx, nếu muốn nó chạy ngay lặp tức thì thêm dấu (), còn muốn chờ đợi thì ko có dấu ()
-    <button onClick={clickMeButton}>Click me</button>
-  );
-}
-
-function CountClickButton() {
-  const [count, setCount] = useState([1, 2, 3, 4, 5, 6, 7, 8]);
-
-  function handleClick() {
-    setCount((count) => {
-      count.push(count.length + 1);
-      return count;
-    });
   }
-  return (
-    <>
-      <button onClick={handleClick}>You clicked {count} time</button>
-    </>
-  );
+  else {
+    return null
+  }
 }
 
-export { App, MyButton, CountClickButton };
+export { DisplayMyNameAndDate, DisplayTrueFlag } 
--- learning-react/src/index.js
+++ learning-react/src/index.js
@@ -1,13 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { App, CountClickButton, MyButton } from "./App";  // Import App.js để hiển thị nội dung
+import { DisplayMyNameAndDate, DisplayTrueFlag } from "./App";
+
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <>
-    <App />
-    <MyButton />
-    <CountClickButton />
+    <DisplayMyNameAndDate />
+    <DisplayTrueFlag />
   </>
 );
 
