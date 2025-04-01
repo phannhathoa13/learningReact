4af7ea5fd7547938d842d0ea3e0f703457864f88
hoafan <Phannhathoa13@gmail.com>
2025-04-01 14:55:17 +0700

    updateDropDownAndRedoLeanReact

--- learning-react/src/components/DropDown/DropDown.jsx
+++ learning-react/src/components/DropDown/DropDown.jsx
@@ -1,23 +1,22 @@
-function DropDown({ values, onSelect }) {
-  const array = [
-    { name: "value1", value: 1 },
-    { name: "value2", value: 2 },
-    { name: "value3", value: 3 },
-  ];
-  function onSelected(e) {
-    const value = e.target.value;
-    onSelect(value);
-    console.log(value);
+function DropDown ({ values, onSelect }) {
+  function onSelected (e) {
+    const id = parseInt(e.target.value)
+    const valueSelect = values.find(value => value.id === id).value
+    onSelect(valueSelect)
   }
   return (
     <div>
-      <select onChange={(e) => onSelected(e)}>
-        {values.map((_value) => {
-          return <option key={_value.id} value={_value.value}>{_value.name}</option>;
+      <select onChange={e => onSelected(e)}>
+        {values.map(_value => {
+          return (
+            <option key={_value.id} value={_value.id}>
+              {_value.name}
+            </option>
+          )
         })}
       </select>
     </div>
-  );
+  )
 }
 
-export { DropDown };
+export { DropDown }
--- learning-react/src/pages/LearnReact/LearnReact.jsx
+++ learning-react/src/pages/LearnReact/LearnReact.jsx
@@ -1,87 +1,88 @@
-import { useState } from "react";
-import { SearchBar } from "../../components/SearchBar/SearchBar";
-import { DisplayListProducts } from "../../components/DisplayListProducts/DisplayListProducts";
-import { DropDown } from "../../components/DropDown/DropDown";
+import { useState } from 'react'
+import { SearchBar } from '../../components/SearchBar/SearchBar'
+import { DisplayListProducts } from '../../components/DisplayListProducts/DisplayListProducts'
+import { DropDown } from '../../components/DropDown/DropDown'
 
 //data
 const PRODUCTS = [
-  { category: "Fruits", price: "$1", stocked: true, name: "Apple" },
-  { category: "Fruits", price: "$1", stocked: true, name: "Dragonfruit" },
-  { category: "Fruits", price: "$2", stocked: false, name: "Passionfruit" },
-  { category: "Vegetables", price: "$2", stocked: true, name: "Spinach" },
-  { category: "Vegetables", price: "$4", stocked: false, name: "Pumpkin" },
-  { category: "Vegetables", price: "$1", stocked: true, name: "Peas" },
-];
-
-const test = [
-  {id: 0, name : "True", value: true},
-  {id: 1, name : "False", value: false},
+  { category: 'Fruits', price: '$1', stocked: true, name: 'Apple' },
+  { category: 'Fruits', price: '$1', stocked: true, name: 'Dragonfruit' },
+  { category: 'Fruits', price: '$2', stocked: false, name: 'Passionfruit' },
+  { category: 'Vegetables', price: '$2', stocked: true, name: 'Spinach' },
+  { category: 'Vegetables', price: '$4', stocked: false, name: 'Pumpkin' },
+  { category: 'Vegetables', price: '$1', stocked: true, name: 'Peas' }
 ]
 
-function filterProducts(products, filterText, priceSelected, isProductStocked) {
-  return products.filter((_product) => {
-    const priceNumber = parseFloat(_product.price.split("$", 2).splice(1, 2));
-
-    const matchProductName = _product.name
-      .toLowerCase()
-      .includes(filterText.toLowerCase());
-    const matchPrice = priceNumber == priceSelected;
-    const matchProductStocked = _product.stocked == isProductStocked;
-
-    if (priceSelected == -1) {
-      return matchProductName && matchProductStocked;
-    } else {
-      return matchProductName && matchProductStocked && matchPrice;
-    }
-  });
-}
-function filterDuplicatePriceProduct() {
-  return PRODUCTS.reduce((acc, item) => {
-    if (!acc.some((test) => test.price === item.price)) {
-      acc.push(item);
-    }
-    return acc;
-  }, []);
-}
-
-
-
-function priceValues() {
-  const listPrice = filterDuplicatePriceProduct();
-  return listPrice.map((_product, index) => {
-    return {
-      id: index,
-      name: _product.price,
-      value: parseFloat(_product.price.split("$", 2).splice(1, 2)),
-    };
-  });
-}
+const stockedValue = [
+  { id: 0, name: 'True', value: true },
+  { id: 1, name: 'False', value: false }
+]
 
 // components
-function LearnReact() {
-  const [filterText, setFilterText] = useState("");
-  const [priceSelected, setPriceSelected] = useState(-1);
-  const [isProductStocked, setIsProductStocked] = useState(false);
+function LearnReact () {
+  const [filterText, setFilterText] = useState('')
+  const [priceSelected, setPriceSelected] = useState(-1)
+  const [isProductStocked, setIsProductStocked] = useState(true)
 
-  console.log(filterDuplicatePriceProduct());
+  const filterProducts = () => {
+    return PRODUCTS.filter(_product => {
+      const productPrice = parseFloat(_product.price.split('$', 2).splice(1, 2))
+
+      const matchName = _product.name
+        .toLocaleLowerCase()
+        .includes(filterText.toLocaleLowerCase())
+
+      const matchPrice = productPrice === priceSelected
+
+      const matchIsStock = isProductStocked === _product.stocked
+
+      if (priceSelected == -1) {
+        return matchName && matchIsStock
+      }
+      return matchPrice && matchName && matchIsStock
+    })
+  }
+
+  const filterDuplicatePriceProduct = () => {
+    return PRODUCTS.reduce((acc, item) => {
+      if (!acc.some(test => test.price === item.price)) {
+        acc.push(item)
+      }
+      return acc
+    }, [])
+  }
+
+  const priceValues = () => {
+    const listPrice = filterDuplicatePriceProduct()
+    const allOption = {
+      name: 'all',
+      value: -1
+    }
+    // map product to data dropdown
+    const value = listPrice.map(_product => {
+      return {
+        name: _product.price,
+        value: parseFloat(_product.price.split('$', 2).splice(1, 2))
+      }
+    })
+    value.unshift(allOption)
+    // map key by index
+    return value.map((value, index) => {
+      return {
+        id: index,
+        ...value
+      }
+    })
+  }
 
   return (
     <div>
       <SearchBar filterText={filterText} setFilterText={setFilterText} />
-      <DisplayListProducts
-        products={filterProducts(
-          PRODUCTS,
-          filterText,
-          priceSelected,
-          isProductStocked
-        )}
-      />
-      <DropDown values={test} />
+      <DropDown values={priceValues()} onSelect={setPriceSelected} />
+      <DropDown values={stockedValue} onSelect={setIsProductStocked} />
+      <DisplayListProducts products={filterProducts()} />
     </div>
-  );
+  )
 }
 
-// logic
-
-//implement
-export { LearnReact };
+export { LearnReact }
