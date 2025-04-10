de8202841dc7b17646771edc2cb61cb47a88231d
hoafan <Phannhathoa13@gmail.com>
2025-04-10 17:54:49 +0700

    delete last Infor

--- learning-react/src/index.js
+++ learning-react/src/index.js
@@ -1,13 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { App, CountClickButton, MyButton } from "./App";  // Import App.js để hiển thị nội dung
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <>
-    <App />
-    <MyButton />
-    <CountClickButton />
+
   </>
 );
 
