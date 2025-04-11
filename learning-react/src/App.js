234577ddbb2a0f6c048e3276c28fc9098d3e6d4c
hoafan <Phannhathoa13@gmail.com>
2025-04-11 16:29:30 +0700

    done task 1 Reac Router

--- learning-react/src/App.js
+++ learning-react/src/App.js
@@ -1,13 +1,19 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import About from './pages/About';
+import Home from './pages/home';
+import Contact from './pages/Contact';
 
 function App() {
     return (
         <BrowserRouter>
             <Routes>
-                <Route path="/" element={<h1>Trang chủ</h1>} />
+                <Route path='/' element={<Home />} />
+                <Route path="/about" element={<About />} />
+                <Route path='/contact' element={<Contact />} />
             </Routes>
         </BrowserRouter>
     );
 }
-export { App }
+
+export { App };
