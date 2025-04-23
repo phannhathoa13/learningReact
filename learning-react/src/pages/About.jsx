000f4b22675edd4dce4e6330ecc7c966f75e7fec
hoafan <Phannhathoa13@gmail.com>
2025-04-23 12:21:33 +0700

    clear old ccode

---
+++ learning-react/src/pages/About.jsx
@@ -0,0 +1,12 @@
+import { Link } from 'react-router-dom'
+
+function About () {
+  return (
+    <>
+      <h1>About </h1>
+      <Link to='/contact'>Contact</Link>
+    </>
+  )
+}
+
+export default About
